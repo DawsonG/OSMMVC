b39d8271434973ed12bf4166986a42ff2873a205
Dawson Goodell <dawson@westlakedesign.com>
2014-08-19 16:13:59 -0400

    Another embarrassing hotfix

--- controller.js
+++ controller.js
@@ -1,13 +1,19 @@
 function Controller() {
+	this.app = this.application = this.express = null;
 	this.res = this.response = null;
+	this.req = this.request = null;
 }
+
+Controller.prototype.setApp = function(app) {
+	this.app = this.application = this.express = app;
+};
 
 Controller.prototype.setRes = function(res) {
 	this.res = this.response = res;
-}
+};
 
 Controller.prototype.setReq = function(req) {
 	this.req = this.request = req;
-}
+};
 
 module.exports = Controller;
--- osmmvc.js
+++ osmmvc.js
@@ -1,15 +1,28 @@
 function OSMMVC() {
 	this.app = null;
+
+
+	this.preload = null;
+	this.postload = null;
 }
 
+OSMMVC.prototype.setBeforeLoad = function(fn) {
+	this.preload = fn;
+};
+
+OSMMVC.prototype.setAfterLoad = function(fn) {
+	this.postload = fn;
+};
+
 OSMMVC.prototype.routing = function(app, routing) {
+	var self = this;
 	var fs = require('fs');
 	var path = require('path'),
     	appDir = path.dirname(require.main.filename);
 
    	this.appDir = appDir;
 	this.app = app;
-	
+
 	if (routing && typeof routing == 'function') {
 		routing(app);
 	} 
@@ -20,11 +33,10 @@ OSMMVC.prototype.routing = function(app, routing) {
 		var input = { params:[], query:[]}; //input stores our extra parameters.
 		var params = req.params[0].split('/');
 
-
 		// Lets get our parameters if and only if they already exist
 		if (params.length >= 1) {
 			controller = params[0];
-			if (req.params.length >= 2) {
+			if (params.length >= 2) {
 				action = params[1];
 			}
 		}
@@ -58,19 +70,35 @@ OSMMVC.prototype.routing = function(app, routing) {
 		}
 		
 		// Make sure the request and response objects are sent to the controller.
+		if (typeof controller.setApp !== 'function') {
+			console.log("Controller does not have basic inherited functions.");
+			console.log("Did you forget to require OSMMVC.Controller or to add your controller to module.exports?");
+			return res.status(404).send('Not found');
+		}
+
+		controller.setApp(app);
 		controller.setReq(req);
 		controller.setRes(res);
 
 		// Get the function
 		var fn = controller[action]; 
 		var params = {};
+
 		if(typeof fn === 'function') {
+			if(typeof self.preload === 'function') {
+				if (self.preload.call(controller, app) === false)
+					return;
+			}
+
 			// Call the function making sure the 'this' context is the controller
 			fn.call(controller, input); // Don't forget to pass our parameters and query string
+
+			if(typeof self.postload === 'function')
+				self.postload.call(controller, app);
 		} else {
 			console.log("INVALID CONTROLLER ACTION");
 			console.log(controller + '[' + action + ']');
-			return res.status(404).send('Not found');;
+			return res.status(404).send('Not found');
 		}
 	});
 }
