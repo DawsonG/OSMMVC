ccf11298ad84a4dc760b85f485cdaa01d4763cae
Dawson Goodell <dawson@westlakedesign.com>
2015-03-18 09:06:11 -0700

    typo in params grabber

--- osmmvc.js
+++ osmmvc.js
@@ -1,6 +1,10 @@
+var fs = require('fs'),
+	path = require('path')
+
 function OSMMVC() {
 	this.app = null;
 	this.appDir = '';
+	this.defaultController = 'main';
 
 	this.preload = null;
 	this.postload = null;
@@ -16,9 +20,41 @@ OSMMVC.prototype.setAfterLoad = function(fn) {
 	this.postload = fn;
 };
 
+OSMMVC.prototype.Route = function(path, controllerPath, arrVerbs) {
+	var self = this;
+	if (!arrVerbs) {
+		arrVerbs = ["get"];
+	}
+
+	if (typeof arrVerbs !== 'Array') {
+		arrVerbs = [arrVerbs];
+	}
+
+	for (var i = 0; i < arrVerbs.length; i++) {
+		self.app[arrVerbs[i]].call(this.app, path, function(req, res) {
+			var input = self._fillInput(null, req);
+			var controllerArr = controllerPath.split('#'),
+				controllerName = controllerArr[0],
+				action = controllerArr.length > 1 ? controllerArr[1] : 'index';
+
+
+			self._callController(controllerName, action, req, res, input);
+		});
+	}
+};
+
 OSMMVC.prototype.config = OSMMVC.prototype.configuration = function(opts) {
-	if (opts && opts.directory)
+	if (opts && opts.directory) {
 		this.appDir = opts.directory;
+	}
+
+	if (opts && opts.additional) {
+		this.addtOpts = opts.additional;
+	}
+
+	if (opts && opts.defaultController) {
+		this.defaultController = opts.defaultController;
+	}
 
 	this.configured = true;
 };
@@ -28,9 +64,7 @@ OSMMVC.prototype.routing = function(app, routing) {
 	if (!self.configured)
 		self.configuration();
 
-	var fs = require('fs');
-	var path = require('path'),
-    	appDir = path.join(path.dirname(require.main.filename), this.appDir);
+	var appDir = path.join(path.dirname(require.main.filename), this.appDir);
 
    	this.appDir = appDir;
 	this.app = app;
@@ -39,11 +73,12 @@ OSMMVC.prototype.routing = function(app, routing) {
 		routing(app);
 	} 
 
-	app.all('/*', function(req, res) {
+	app.all("/*", function(req, res) {
 		var controller = null;
 		var action = null;
-		var input = { params:[], query:[], body:[], user:{} }; //input stores our extra parameters.
 		var params = req.params[0].split('/');
+		var input = self._fillInput(params, req); //input stores our extra parameters.
+
 
 		// Lets get our parameters if and only if they already exist
 		if (params.length >= 1) {
@@ -55,77 +90,110 @@ OSMMVC.prototype.routing = function(app, routing) {
 
 		// If we don't have a any parameters we go to the defaults.
 		if (!controllerName)
-			controllerName = "main";
+			controllerName = self.defaultController;
 
 		if (!action)
 			action = "index";
 
+		self._callController(controllerName, action, req, res, input);
+	});
+}
+
+/* === Private Functions === */
+function merge_options(obj1, obj2){
+	var rtn = {};
+	for (var attrname in obj1) { rtn[attrname] = obj1[attrname]; }
+	for (var attrname in obj2) { rtn[attrname] = obj2[attrname]; }
+	return rtn;
+}
+
+OSMMVC.prototype._fillInput = function(params, req) {
+	var rtn = { params:[], query:[], body:[], user:{} };
+
+	console.log(params);
+	console.log(params.length);
+	// Get any additional parameters
+	if (!params && req.params) {
+		rtn.params = req.params;
+	} else {
 		// Get any additional parameters
 		if (params.length > 2) {
 			for (var i = 2; i < params.length; i++) {
-				input.params.push(req.params[i]);
+				rtn.params.push(params[i]);
 			}
 		}
+	}
 
-		// Add the query string as well
-		if (req.query) {
-			input.query = req.query;
+	// Add the query string as well
+	if (req.query) {
+		rtn.query = req.query;
+	}
+
+	// Add our user if need be
+	if (req.user) {
+		rtn.user = req.user;
+	}
+
+	// If we are posting we'll need the form body too.
+	if (req.method == 'POST' || req.method == 'PUT') {
+		rtn.body = req.body;
+	}
+
+	return rtn;
+};
+
+OSMMVC.prototype._callController = function(controllerName, action, req, res, input) {
+	var self = this;
+
+	// Find the controller, make sure it exists, and get it
+	if (fs.existsSync(self.appDir + '/controllers/' + controllerName + '.js')) {
+		controller = require(self.appDir + '/controllers/' + controllerName);
+	} else {
+		console.log("INVALID CONTROLLER PATH");
+		console.log('ORIGINAL - ' + req.params[0] + '   PROCESSED - controllers/' + controllerName + '.js');
+		return res.status(404).send('Not found');
+	}
+	
+	// Make sure the request and response objects are sent to the controller.
+	if (typeof controller.setApp !== 'function') {
+		console.log("Controller does not have basic inherited functions.");
+		console.log("Did you forget to require OSMMVC.Controller or to add your controller to module.exports?");
+		return res.status(404).send('Not found');
+	}
+
+	controller.setApp(self.app);
+	controller.setReq(req);
+	controller.setRes(res);
+	var opts = {
+		controller: controllerName,
+		action: action
+	};
+
+	if (typeof self.addtOpts === 'function') {
+		opts = merge_options(opts, self.addtOpts.call(controller));
+	}
+
+	controller.setOpts(opts);
+
+	// Get the function
+	var fn = controller[action];
+
+	if(typeof fn === 'function') {
+		if(typeof self.preload === 'function') {
+			if (self.preload.call(controller, self.app) === false)
+				return;
 		}
 
-		// Add our user if need be
-		if (req.user) {
-			input.user = req.user;
-		}
+		// Call the function making sure the 'this' context is the controller
+		fn.call(controller, input, self.app); // Don't forget to pass our parameters and query string
 
-		// If we are posting we'll need the form body too.
-		if (req.method == 'POST' || req.method == 'PUT') {
-			input.body = req.body;
-		}
-
-		// Find the controller, make sure it exists, and get it
-		if (fs.existsSync(appDir + '/controllers/' + controllerName + '.js')) {
-			controller = require(appDir + '/controllers/' + controllerName);
-		} else {
-			console.log("INVALID CONTROLLER PATH");
-			console.log('ORIGINAL - ' + req.params[0] + 'PROCESSED - controllers/' + controller + '.js');
-			return res.status(404).send('Not found');
-		}
-		
-		// Make sure the request and response objects are sent to the controller.
-		if (typeof controller.setApp !== 'function') {
-			console.log("Controller does not have basic inherited functions.");
-			console.log("Did you forget to require OSMMVC.Controller or to add your controller to module.exports?");
-			return res.status(404).send('Not found');
-		}
-
-		controller.setApp(app);
-		controller.setReq(req);
-		controller.setRes(res);
-		controller.setOpts({
-			controller: controllerName,
-			action: action
-		});
-
-		// Get the function
-		var fn = controller[action];
-
-		if(typeof fn === 'function') {
-			if(typeof self.preload === 'function') {
-				if (self.preload.call(controller, app) === false)
-					return;
-			}
-
-			// Call the function making sure the 'this' context is the controller
-			fn.call(controller, input); // Don't forget to pass our parameters and query string
-
-			if(typeof self.postload === 'function')
-				self.postload.call(controller, app);
-		} else {
-			console.log("INVALID CONTROLLER ACTION");
-			console.log(controller + '[' + action + ']');
-			return res.status(404).send('Not found');
-		}
-	});
-}
+		if(typeof self.postload === 'function')
+			self.postload.call(controller, self.app);
+	} else {
+		console.log("INVALID CONTROLLER ACTION");
+		console.log(controller + '[' + action + ']');
+		return res.status(404).send('Not found');
+	}
+};
 
 var mvc = module.exports = exports = new OSMMVC;
